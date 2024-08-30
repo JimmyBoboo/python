d36226ffd2be00cee8d68807f22ed9fdc867e339
JimmyBoboo <jimmybui1234@hotmail.com>
2024-08-30 17:14:12 +0200

    oppgaver 4
    gjort fram til oppgave 4

---
+++ O1/oppgave4.py
@@ -0,0 +1,21 @@
+#Lag tre variabler: a=6, b=3, c=2
+
+#Definer variablene
+a = 6
+b = 3
+c = 2
+
+#Regnestykket
+result_a = a + b * c        #a + b * c 
+result_b = (a + b) * c      #(a + b) * c
+result_c = a / b / c        #a / b / c
+result_d = a / (b / c)      #a / (b / c)
+
+print(f"{a} + {b} * {c} = " + str(result_a))
+print(f"({a} + {b}) * {c} =", result_b)
+print(f"{a} / {b} / {c} =", result_c)
+print(f"{a} / ({b} / {c}) =", result_d)
+
+
+
+
