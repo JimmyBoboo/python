18a41953dd1ff2cca569a98d58b77867358cf94b
JimmyBoboo <jimmybui1234@hotmail.com>
2024-09-19 23:07:35 +0200

    Update Oppgave2.py

--- O2/Oppgave2.py
+++ O2/Oppgave2.py
@@ -1,9 +1,10 @@
 #Printer ut alle oddetallene fra 9-101. 
+#For-løkke
 for nummer in range(9, 102, 2):
     print(nummer)
 
 
-
+#While-løkke
 print("Her starter While-lokke!")
 Tall = 9
 while Tall <= 101:
@@ -12,5 +13,6 @@ while Tall <= 101:
 
 else:
     print("Her stopper det!")
+
 
 
