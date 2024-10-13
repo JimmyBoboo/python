8cdda829faab04ed0fbac5023c54ec0d37b11ca8
Jimmy Bui <jimmybui1234@hotmail.com>
2024-10-13 15:56:29 +0200

    o34

---
+++ O3/Oppgave5.1.py
@@ -0,0 +1,22 @@
+# Opprett en liste med filmer, hvor hver film har en egen dictionary med dataene om en film. 
+filmer = [
+    {'name': 'Inception', 'year': 2010, 'rating': 8.7},
+    {'name': 'Inside Out', 'year': 2015, 'rating': 8.1},
+    {'name': 'Con Air', 'year': 1997, 'rating': 6.9}
+]
+
+#for film in filmer:
+    #print(f"{film['name']}, kom ut i {film['year']} og ratingen er {film['rating']}.")
+    
+def leggTilFilm(filmer, name, year, rating=5.0):
+    filmer.append({'name': name, 'year': year, 'rating': rating})
+
+leggTilFilm(filmer, 'The martian', 2015, 8.9)
+leggTilFilm(filmer, 'Notebook', 2004, 8.3)
+leggTilFilm(filmer, 'Gemini man', 2019, )
+
+for film in filmer:
+    print(f"{film['name']}, kom ut i {film['year']} og ratingen er {film['rating']}.")
+
+
+    
