506c8d019126bc14705eabf34b10d3e7cd71e3f7
Jimmy Bui <jimmybui1234@hotmail.com>
2024-10-13 12:48:54 +0200

    commit

--- O3/Oppgave1.py
+++ O3/Oppgave1.py
@@ -2,15 +2,20 @@ student = {
     "fornavn" : "Ola", 
     "etternavn" : "Nordmann",
     "favorittfag" : "Programmering 1" 
-} 
-
+}
+#Skriv ut studentes navn og etternavn. 
 x = student ["fornavn"]
 y = student ["etternavn"]
+print("****1.*****")
 print(x, y)
-
+#Endre studentes favoritt fag til ITF10219 Programmering 1.
+print("*****2.*****")
 student["favorittfag"] = "ITF10219 Programmering 1"
 z = student["favorittfag"]
 print (z)
-
+#Legge til alder.
+print("****3.****")
 student["alder"] = 21
 print(student)
+
+
