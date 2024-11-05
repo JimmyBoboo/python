89adf67549b75ce54664f4017d7b715da910e8fe
JimmyBoboo <jimmybui1234@hotmail.com>
2024-11-05 12:36:07 +0100

    04
    Endra på prints, og prøvde å gjøre if tester.
    OBS: Fiks slik at if testene er riktig. At hvis dealer får blackjack, print ut "Dealer har fått blackjack"

--- GUI/qt_vindumedbilde.py
+++ GUI/qt_vindumedbilde.py
@@ -1,26 +1,30 @@
-# Vårt første GUI
-
-# Vi trenger å importere et par klasser
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt6.QtGui import QPixmap
 import sys
 
-# Vi må alltid ha med et QApplication-objekt når vi lager et GUI. Gir det navnet app. sys.argv lar oss sende med parametere dersom vi bruker kommandolinjen.
-# (Dersom vi vet at vi aldri kommer til å starte via kommandolinjen kan vi istedenfor sys.argv sende med [] (en tom liste) )
-app = QApplication(sys.argv)    # må alltid med
+# Lager en klasse, Vindu, som skal arve fra klassen QWidget.
+# Denne klassen er tenkt å inneholde all kode for å lage GUI for vinduet vårt.
+class Vindu(QWidget):
 
-# Vi lager et vindu. Vinduet lager vi her som et QWidget-objekt
-vindu = QWidget()
+    def __init__(self):
+        super().__init__()                      
+        self.setWindowTitle('Enkelt vindu')     
+        self.resize(300, 300)                  
 
-# Klassen QWidget har en rekke metoder, her bruker vi metoden setWindowTitle() til å gi vinduet en tittel
-vindu.setWindowTitle('Hello world!')
-# Vi bestemmer størrelsen vinduet skal ha når det vises (vi kan derimot 'dra i hjørnene' og endre størrelsen når vi kjører koden)
-vindu.resize(300, 100)
-# Flytter vinduet til en ny startposisjonskoordinater. 
-# Merk at første verdi, 700, er avstand i piksler fra venstre side av skjermen. Andre verdi er avstand fra toppen av skjermen.
-vindu.move(700, 300)
+        label1 = QLabel()                      
+        label1.setText("Hello world!")          
 
-# For å vise vinduet må vi kalle show(), ellers vil det ikke vises når vi kjører koden
-vindu.show()
+        label_img = QLabel()                    
+        pixmap = QPixmap('pony.jpg')            
+        label_img.setPixmap(pixmap) 
+                    
 
-# Egentlig vises ikke appen vår før følgende linje blir kjørt. Det er app.exec() som sørger for dette, og må derfor alltid være med når vi lager GUI. sys.exit() sørger for at appen vår "leker fint" med maskinen vår dersom vi kjører den fra kommandolinjen
-sys.exit(app.exec())    # må alltid med
+
+
+
+app = QApplication(sys.argv)                   
+vindu = Vindu()                                
+vindu.show()                                    
+sys.exit(app.exec())                            
+
+# To be continued..
--- O4/test2.py
+++ O4/test2.py
@@ -16,13 +16,20 @@ while True:
     print(f"\n The cards have been dealt. You have a {player[0]} and a {player[1]}, \n with a total value of: {bjm.calculate_hand_value(player)}")
     print(f"\n The dealers visible card is a {dealer[0]}, \n with a value of: {bjm.get_card_value(dealer[0])}")
 
+    # Muligheter for hva som skjer
     if bjm.calculate_hand_value(player) == 21:
         print("Congratulations, you have blackjack!")
         
+    elif bjm.calculate_hand_value(dealer) == 21:
+        print("Dealeren fikk blackjack, du har tapt!")
+    
+    elif bjm.calculate_hand_value(dealer) > bjm.calculate_hand_value(player):
+        print(f"Dealeren fikk {bjm.calculate_hand_value(dealer)}, du taper!")
+        
+        
     else:
         while True:
-            print("1. Hit")
-            print("2. Stand")
+            print("\n Do you wish to hit or stand? \n 1 - Hit \n 2 - Stand")
             choice = input("> ")
             
             
@@ -45,18 +52,20 @@ while True:
                     
                 
             elif choice == "2":
-                print(f"You have a {player[0]} and a {player[1]}  with a total value of {bjm.calculate_hand_value(player)}")
-                print(f"The dealers has the cards {dealer[0]} and {dealer[1]} with a value of {bjm.calculate_hand_value(dealer)}")
-                bjm.print_result(bjm.calculate_hand_value(player), bjm.calculate_hand_value(dealer))
+                # print(f"You have a {player[0]} and a {player[1]}  with a total value of {bjm.calculate_hand_value(player)}")
+                # print(f"The dealers has the cards {dealer[0]} and {dealer[1]} with a value of {bjm.calculate_hand_value(dealer)}")
+                
+                while bjm.calculate_hand_value(dealer) < 17:
+                    nytt_kort_Dealer = bjm.get_new_card(cards)
+                    dealer.append(nytt_kort_Dealer)
+                    print(f"Dealeren fikk {nytt_kort_Dealer}, Dealeren har totalt: {bjm.calculate_hand_value(dealer)}")
+                # bjm.print_result(bjm.calculate_hand_value(player), bjm.calculate_hand_value(dealer))
                 break
             
            
             else:
                 print("Invalid choice")
                 
-            
-            
-            
                 
         play_again = input("Do you want to play again? (yes/no) > ").lower()
         if play_again == "yes":
